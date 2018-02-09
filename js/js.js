501a7ecc5bd867ae2d98003ccae3546397c808d3
Frontend Update <arte@update.pe>
2018-02-09 18:07:20 -0500

    Initial commit

---
+++ js/js.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("#menucomida").click(function(){
+        $(".menucomida").toggle();
+    });
+    
+    
+        $("#hijo1").click(function(){
+        $(".hijo1").toggle();
+    });
+    
+        $("#hijo2").click(function(){
+        $(".hijo2").toggle();
+        
+    });
+    
+});
---
+++ menu.php
@@ -0,0 +1,253 @@
+  <!DOCTYPE html>
+  <html lang="es">
+    <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <title>Titulo del Sitio</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="description" content="DESCRIPCION DEL SITIO" />
+      <meta name="keywords" content="PALABRAS CLAVER" />
+      <meta name="author" content="luis alberto bernal fuentes" />
+
+        <!-- Facebook and Twitter integration -->
+      <meta property="og:title" content=""/>
+      <meta property="og:image" content=""/>
+      <meta property="og:url" content=""/>
+      <meta property="og:site_name" content=""/>
+      <meta property="og:description" content=""/>
+      <meta name="twitter:title" content="" />
+      <meta name="twitter:image" content="" />
+      <meta name="twitter:url" content="" />
+
+      <!--jqueryhideshow-->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+      <link rel="stylesheet" type="text/css" href="css/animate.css">
+      <link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+      <link rel="stylesheet" type="text/css" href="css/materialize.css">
+      <link rel="stylesheet" type="text/css" href="css/style.css">
+
+      <!--normalizecss-->
+      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.css">
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+
+      <!--fontawesome fuentes-->
+      <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+
+
+    </head>
+
+    <body>
+          <!--contenidoBODY-->
+
+
+
+
+<!--menuresponsive-->
+
+  <ul id="slide-out" class="side-nav">
+    <li><div class="user-view">
+      <div class="background bgcolor" >
+
+      </div>
+              <a href="#!user"><i class="material-icons grande" style="font-size: 72px;">supervisor_account</i></a>
+              <a href="#!name"><span class="white-text name">Nombre Padre</span></a><a href="">Logout</a>
+              <a href="#!email"><span class="white-text email">jdandturk@gmail.com</span></a>
+    </div>
+    </li>
+    <li class="thin"><a href="index.php">Inicio</a></li>
+       <li class="thin"><a href="menu.php">Menus</a></li>
+              <ul class="collapsible collapsible-accordion">
+                <li>
+                  <a class="collapsible-header thin">Mis hijos <i class="material-icons">arrow_drop_down</i></a>
+                  <div class="collapsible-body">
+                    <ul>
+                          <li class="thin"><a href="#test2">Alan</a></li>
+                          <li class="thin"><a href="#test3">Gionella</a></li>
+                    </ul>
+                  </div>
+                </li>
+              </ul>
+            <li class="thin"><a href="#test4">Agregar Hijo</a></li>
+            <li class="thin"><a href="#test5">Agregar Menus</a></li>
+        <li class="thin"><a href="#test6">Agregar Colegio</a></li>
+  </ul>
+
+<!--menuresponsive-->
+<!--menunormal-->
+
+  <nav class="bgcolor">
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Logo</a>
+      <ul id="nav-mobile" class="right ">
+        <li><a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a></li>
+
+      </ul>
+    </div>
+  </nav>
+
+<!--menunormal-->
+
+
+
+<div id="container">
+
+  <div id="menu">
+
+    <ul id="slide-out" class="side-nav fixed tabs">
+      <li><div class="user-view">
+            <div class="background bgcolor">
+                
+              </div>
+              <a href="#!user"><i class="material-icons grande" style="font-size: 72px;">supervisor_account</i></a>
+              <a href="#!name"><span class="white-text name">Nombre Padre</span></a><a href="">Logout</a>
+              <a href="#!email"><span class="white-text email">jdandturk@gmail.com</span></a>
+            </div>
+      </li>
+      <li class="thin"><a href="#test1">Menus</a></li>
+        <ul class="collapsible collapsible-accordion">
+          <li>
+            <a class="collapsible-header thin">Mis hijos <i class="material-icons">arrow_drop_down</i></a>
+            <div class="collapsible-body">
+              <ul>
+                    <li class="thin"><a href="#test2">Alan</a></li>
+                    <li class="thin"><a href="#test3">Gionella</a></li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      <li class="thin"><a href="#test4">Agregar Hijo</a></li>
+      <li class="thin"><a href="#test5">Agregar Menus</a></li>
+       <li class="thin"><a href="#test6">Agregar Colegio</a></li>
+    </ul>
+  </div>
+
+<!--secciomenus-->
+  <div id="test1">
+      <div class="row">
+
+        <p>lunes 12-12-12</p>
+        <div class="col s12 m4 l4">
+          <div class="card small">
+            <div class="card-image">
+              <img src="http://via.placeholder.com/150">
+              <span class="card-title">Titulo Menu</span>
+            </div>
+            <div class="card-content">
+              <p>Descripcion breve del menu seleccionado.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">Alan</a>
+              <a href="#">Gionella</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col s12 m2 l2">
+          <div class="card small">
+            <div class="card-image">
+              <img src="http://via.placeholder.com/150">
+              <span class="card-title">Titulo Menu</span>
+            </div>
+            <div class="card-content">
+              <p>Descripcion breve del menu seleccionado.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">Alan</a>
+              <a href="#">Gionella</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col s12 m2 l2">
+          <div class="card small">
+            <div class="card-image">
+              <img src="http://via.placeholder.com/150">
+              <span class="card-title">Titulo Menu</span>
+            </div>
+            <div class="card-content">
+              <p>Descripcion breve del menu seleccionado.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">Alan</a>
+              <a href="#">Gionella</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col s12 m2 l2">
+            <div class="card small">
+              <div class="card-image">
+                <img src="http://via.placeholder.com/150">
+                <span class="card-title">Titulo Menu</span>
+              </div>
+              <div class="card-content">
+                <p>Descripcion breve del menu seleccionado</p>
+              </div>
+              <div class="card-action">
+                <a href="#">Alan</a>
+                <a href="#">Gionella</a>
+              </div>
+            </div>
+        </div>
+
+        <div class="col s12 m2 l2">
+          <div class="card small">
+            <div class="card-image">
+              <img src="http://via.placeholder.com/150">
+              <span class="card-title">Titulo Menu</span>
+            </div>
+            <div class="card-content">
+              <p>Descripcion breve del menu seleccionado.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">Alan</a>
+              <a href="#">Gionella</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+  </div>
+<!--secciomenus-->
+ 
+
+
+
+
+
+
+    
+    
+</div>
+
+          <!--contenidoBODY-->
+                <!--Import jQuery before materialize.js-->
+                <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+                <script type="text/javascript" src="js/materialize.min.js"></script>
+                  <script type="text/javascript" src="js/materialize.js"></script>
+                  <script type="text/javascript" src="js/js.js"></script>
+
+                <script type="text/javascript">
+                $(document).ready(function() {
+
+                      $('.button-collapse').sideNav({
+                          menuWidth: 300, // Default is 300
+                          edge: 'left', // Choose the horizontal origin
+                          closeOnClick: false, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+                          draggable: true // Choose whether you can drag to open on touch screens,
+                        }
+                      ); 
+
+                });
+                </script>
+    </body>
+  </html>
