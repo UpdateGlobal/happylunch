be6aae4553a5b3e65cd32c52fac5803d1c60e24a
Frontend Update <arte@update.pe>
2018-02-12 18:01:00 -0500

    12 febrero

--- index.php
+++ index.php
@@ -25,8 +25,8 @@
 
       <!--Import Google Icon Font-->
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+      <!--Import materialize.css
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>-->
       <link rel="stylesheet" type="text/css" href="css/animate.css">
       <link rel="stylesheet" type="text/css" href="css/font-awesome.css">
       <link rel="stylesheet" type="text/css" href="css/materialize.css">
@@ -45,25 +45,20 @@
 
     </head>
 
-    <body>
-          <!--contenidoBODY-->
-
-
-
-
-<!--menuresponsive-->
-
+<body>
+<!--contenidoBODY-->
+<!--mnuresponsive-->
   <ul id="slide-out" class="side-nav">
     <li><div class="user-view">
       <div class="background bgcolor" >
 
       </div>
-              <a href="#!user"><i class="material-icons grande" style="font-size: 72px;">supervisor_account</i></a>
-              <a href="#!name"><span class="white-text name">Nombre Padre</span></a><a href="">Logout</a>
-              <a href="#!email"><span class="white-text email">jdandturk@gmail.com</span></a>
+       <a href="#!user"><i class="material-icons grande" style="font-size: 72px;">supervisor_account</i></a>
+       <a href="#!name"><span class="white-text name">Nombre Padre</span></a><a href="">Logout</a>
+       <a href="#!email"><span class="white-text email">jdandturk@gmail.com</span></a>
     </div>
     </li>
-       <li class="thin"><a href="menu.php">Menus</a></li>
+       <li class="thin"><a href="movil/menu.php">Menus</a></li>
               <ul class="collapsible collapsible-accordion">
                 <li>
                   <a class="collapsible-header thin">Mis hijos <i class="material-icons">arrow_drop_down</i></a>
@@ -76,7 +71,7 @@
                 </li>
               </ul>
             <li class="thin"><a href="#test4">Agregar Hijo</a></li>
-            <li class="thin"><a href="#test5">Agregar Menus</a></li>
+            <li class="thin"><a href="#test5">Crear Menus</a></li>
         <li class="thin"><a href="#test6">Agregar Colegio</a></li>
   </ul>
 
@@ -123,103 +118,216 @@
           </li>
         </ul>
       <li class="thin"><a href="#test4">Agregar Hijo</a></li>
-      <li class="thin"><a href="#test5">Agregar Menus</a></li>
+      <li class="thin"><a href="#test5">Crear Menus</a></li>
        <li class="thin"><a href="#test6">Agregar Colegio</a></li>
     </ul>
   </div>
 
-<!--secciomenus-->
+<!--
+======================================================================
+==                                                                  ==
+==                AQUI INICIA LA SECCION DE MENUS                   ==
+==                                                                  ==
+======================================================================
+-->
+
   <div id="test1" style="display: none;">
-      <div class="row">
 
-        <p>lunes 12-12-12</p>
-        <div class="col s12 m4 l4">
-          <div class="card small">
-            <div class="card-image">
-              <img src="http://via.placeholder.com/150">
-              <span class="card-title">Titulo Menu</span>
-            </div>
-            <div class="card-content">
-              <p>Descripcion breve del menu seleccionado.</p>
-            </div>
-            <div class="card-action">
-              <a href="#">Alan</a>
-              <a href="#">Gionella</a>
+  <ul class="collapsible" data-collapsible="accordion">
+    <li>
+      <div class="collapsible-header"><i class="material-icons">room_service</i>Lunes - 12-12-12</div>
+      <div class="collapsible-body">
+
+        <div class="row">
+          <div class="col s12 m3 l3" style="width: 20%;">
+            <div class="card">
+                <div class="card-image waves-effect waves-block waves-light">
+                  <img class="activator" src="http://materializecss.com/images/office.jpg">
+                </div>
+                <div class="card-content">
+                  <span class="activator grey-text text-darken-4">Nombre Menu<i class="material-icons right">event_note</i></span>
+                </div>
+                <div class="card-reveal">
+                  <span class="card-title grey-text text-darken-4 nombreson">Para Quienes?<i class="material-icons right">close</i></span>
+                  <br>
+                  <p style="font-size: 8px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua.</p>
+
+                 
+                  <p class="btn chip">Alan</p>
+                  <p class="btn chip">Gionella</p>
+                  <p class="btn chip">Raul</p>
+                </div>
             </div>
           </div>
-        </div>
-
-        <div class="col s12 m2 l2">
-          <div class="card small">
-            <div class="card-image">
-              <img src="http://via.placeholder.com/150">
-              <span class="card-title">Titulo Menu</span>
-            </div>
-            <div class="card-content">
-              <p>Descripcion breve del menu seleccionado.</p>
-            </div>
-            <div class="card-action">
-              <a href="#">Alan</a>
-              <a href="#">Gionella</a>
-            </div>
-          </div>
-        </div>
-
-        <div class="col s12 m2 l2">
-          <div class="card small">
-            <div class="card-image">
-              <img src="http://via.placeholder.com/150">
-              <span class="card-title">Titulo Menu</span>
-            </div>
-            <div class="card-content">
-              <p>Descripcion breve del menu seleccionado.</p>
-            </div>
-            <div class="card-action">
-              <a href="#">Alan</a>
-              <a href="#">Gionella</a>
+          <div class="col s12 m3 l3" style="width: 20%;">
+            <div class="card">
+                <div class="card-image waves-effect waves-block waves-light">
+                  <img class="activator" src="http://materializecss.com/images/office.jpg">
+                </div>
+                <div class="card-content">
+                  <span class="activator grey-text text-darken-4">Nombre Menu<i class="material-icons right">event_note</i></span>
+                </div>
+                <div class="card-reveal">
+                  <span class="card-title grey-text text-darken-4 nombreson">Para Quienes?<i class="material-icons right">close</i></span>
+                  <p class="btn chip">Alan</p>
+                  <p class="btn chip">Gionella</p>
+                  <p class="btn chip">Raul</p>
+                </div>
             </div>
           </div>
-        </div>
-
-        <div class="col s12 m2 l2">
-            <div class="card small">
-              <div class="card-image">
-                <img src="http://via.placeholder.com/150">
-                <span class="card-title">Titulo Menu</span>
-              </div>
-              <div class="card-content">
-                <p>Descripcion breve del menu seleccionado</p>
-              </div>
-              <div class="card-action">
-                <a href="#">Alan</a>
-                <a href="#">Gionella</a>
-              </div>
-            </div>
-        </div>
-
-        <div class="col s12 m2 l2">
-          <div class="card small">
-            <div class="card-image">
-              <img src="http://via.placeholder.com/150">
-              <span class="card-title">Titulo Menu</span>
-            </div>
-            <div class="card-content">
-              <p>Descripcion breve del menu seleccionado.</p>
-            </div>
-            <div class="card-action">
-              <a href="#">Alan</a>
-              <a href="#">Gionella</a>
+          <div class="col s12 m3 l3" style="width: 20%;">
+            <div class="card">
+                <div class="card-image waves-effect waves-block waves-light">
+                  <img class="activator" src="http://materializecss.com/images/office.jpg">
+                </div>
+                <div class="card-content">
+                  <span class="activator grey-text text-darken-4">Nombre Menu<i class="material-icons right">event_note</i></span>
+                </div>
+                <div class="card-reveal">
+                  <span class="card-title grey-text text-darken-4 nombreson">Para Quienes?<i class="material-icons right">close</i></span>
+                  <p class="btn chip">Alan</p>
+                  <p class="btn chip">Gionella</p>
+                  <p class="btn chip">Raul</p>
+                </div>
             </div>
           </div>
-        </div>
+          <div class="col s12 m3 l3" style="width: 20%;">
+            <div class="card">
+                <div class="card-image waves-effect waves-block waves-light">
+                  <img class="activator" src="http://materializecss.com/images/office.jpg">
+                </div>
+                <div class="card-content">
+                  <span class="activator grey-text text-darken-4">Nombre Menu<i class="material-icons right">event_note</i></span>
+                </div>
+                <div class="card-reveal">
+                  <span class="card-title grey-text text-darken-4 nombreson">Para Quienes?<i class="material-icons right">close</i></span>
+                  <p class="btn chip">Alan</p>
+                  <p class="btn chip">Gionella</p>
+                  <p class="btn chip">Raul</p>
+                </div>
+            </div>
+          </div>
+          <div class="col s12 m3 l3" style="width: 20%;">
+            <div class="card">
+                <div class="card-image waves-effect waves-block waves-light">
+                  <img class="activator" src="http://materializecss.com/images/office.jpg">
+                </div>
+                <div class="card-content">
+                  <span class="activator grey-text text-darken-4">Nombre Menu<i class="material-icons right">event_note</i></span>
+                </div>
+                <div class="card-reveal">
+                  <span class="card-title grey-text text-darken-4 nombreson">Para Quienes?<i class="material-icons right">close</i></span>
+                  <p class="btn chip">Alan</p>
+                  <p class="btn chip">Gionella</p>
+                  <p class="btn chip">Raul</p>
+                </div>
+            </div>
+          </div>
 
+        </div>
       </div>
-  </div>
-<!--secciomenus-->
+    </li>
+    
+
+  </ul>
+</div>
+<!--
+======================================================================
+==                                                                  ==
+==                AQUI TERMINA LA SECCION DE MENUS                   ==
+==                                                                  ==
+======================================================================
+-->
+<!--
+======================================================================
+==                                                                  ==
+==                AQUI INICIA LA SECCION DE HIJOS                   ==
+==                                                                  ==
+======================================================================
+-->
   <div id="test2" style="display: none;">Mi Hijo Alan</div>
+  <!--
+======================================================================
+==                                                                  ==
+==                AQUI TERMINA LA SECCION DE MENUS                   ==
+==                                                                  ==
+======================================================================
+-->
   <div id="test3" style="display: none;">Mi Hija Gionella</div>
   <div id="test4" style="display: none;">Registar hijo aqui formulario</div>
-  <div id="test5" style="display: none;">Registar Menus</div>
+<!--
+======================================================================
+==                                                                  ==
+==           AQUI INICIA LA SECCION DE REGISTRO MENU                ==
+==                                                                  ==
+======================================================================
+-->
+<div id="test5" style="display: none;">
+
+      <div class="row">
+        <h3 class="thin">Agregar Menu</h3>
+        <div class="col s12 m6 l6" align="left">
+            <form action="#">
+                   <div class="input-field">
+                    <input id="icon_prefix" type="text" class="validate" data-length="12  ">
+                    <label for="icon_prefix">Nombre Menu</label>
+                  </div>
+                  <div class="input-field">
+                    <textarea id="textarea1" class="materialize-textarea"></textarea>
+                    <label for="textarea1">Descripcion</label>
+                  </div>
+                  <div class="file-field input-field">
+                    <div class="btn">
+                      <span>Archivo</span>
+                      <input type="file">
+                    </div>
+                    <div class="file-path-wrapper">
+                      <input class="file-path validate" type="text">
+                    </div>
+                  </div>
+              </div>
+              <div class="col s12 m6 l6">
+                <label for="textarea1">Fecha publicacion menu</label>
+                 <input type="text" class="datepicker">
+              </div>  
+             
+            </div>
+            <div class="container" align="center">
+                <input type="submit" name="submit" id="submit" class="btn" value="Registar">
+            </div>
+          </form>
+          <br>   
+
+               <div class="divider"></div>
+
+            <h3 class="thin">Consultar</h3>  
+            <div class="row">
+            <div class="col s12 m6 l6">
+              <form action="">
+                <label for="textarea1">Desde</label>
+                <input type="text" class="datepicker">
+            </div>
+            <div class="col s12 m6 l6">
+                 <label for="textarea1">Hasta</label>
+                 <input type="text" class="datepicker">
+            </div>
+              <input type="submit" name="submit" id="submit" class="btn" value="Consultar fechas">
+              </form> 
+            </div> 
+</div> 
+<!--
+======================================================================
+==                                                                  ==
+==           AQUI INICIA LA SECCION DE REGISTRO MENU                ==
+==                                                                  ==
+======================================================================
+-->
+
+
+
+
+
   <div id="test6" style="display: none;">Agregar Colegio</div>
 
 
@@ -241,6 +349,15 @@
 
                 <script type="text/javascript">
                 $(document).ready(function() {
+
+                    $('.datepicker').pickadate({
+                      selectMonths: true, // Creates a dropdown to control month
+                      selectYears: 15, // Creates a dropdown of 15 years to control year,
+                      today: 'Hoy',
+                      clear: 'Limpiar',
+                      close: 'Ok',
+                      closeOnSelect: false // Close upon selecting a date,
+                    });
 
                       $('.button-collapse').sideNav({
                           menuWidth: 300, // Default is 300
